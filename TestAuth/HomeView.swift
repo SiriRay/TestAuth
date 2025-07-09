29349906646ff3bece7c309aa4c8d136d72e422b
Siriiii <siriii@Mac.lan>
2025-07-08 17:25:11 -0700

    Initial commit

---
+++ TestAuth/HomeView.swift
@@ -0,0 +1,35 @@
+
+
+import SwiftUI
+import Firebase
+import FirebaseAuth
+
+struct HomeView: View {
+    var body: some View {
+        VStack{
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            
+            Button{
+                Task{
+                    do{
+                        try await logout()
+                    }
+                }
+                } label: {
+                    Text("Log Out").padding(8)
+                }.buttonStyle(.borderedProminent)
+
+                
+        
+            }
+            .padding()
+        }
+    
+    func logout() async throws{
+        try Auth.auth().signOut()
+    }
+}
+
+#Preview {
+    HomeView()
+}
